9ccaba479f80c2021fdb985837ba7822d7e28d8f
Geovana Moreira de Oliveira <geovanamoreira907@gmail.com>
2022-08-09 08:40:59 -0300

    09-08

--- src/app/alunos/alunos-routing.module.ts
+++ src/app/alunos/alunos-routing.module.ts
@@ -1,7 +1,12 @@
+import { ListaalunosComponent } from './listaalunos/listaalunos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ListaalunosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
